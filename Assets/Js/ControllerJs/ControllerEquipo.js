8fc886dbcf1bbabbba5b67b9d8acce8d1dc063b1
FernanPro18 <fernandoaraujo081@gmail.com>
2018-03-15 17:48:02 -0500

    errores

--- Assets/Js/ControllerJs/ControllerEquipo.js
+++ Assets/Js/ControllerJs/ControllerEquipo.js
@@ -1,11 +1,19 @@
 angular.module('EquipoController', [])
-.controller("EquipoController", function ($scope, $http) {
-  $scope.Equipos = [];
-  $http.get('https://api-net.herokuapp.com/api/Equipos')
-    .then(function (data) {
-      $scope.Equipos = data.data;
-      console.log(data.data);
-    }, function (err) {
-      
-    });
-});
+  .service('peticion', function ($http, $scope) {
+    this.get = function () {
+      $http({
+          method: "GET",
+          url: "https://api-net.herokuapp.com/api/Equipos",
+          timeout: 15 * 1000
+        })
+        .then(function (data) {
+          $scope.Equipos = data.data;
+        }, function (data, status, headers, config) {
+          return status;
+        });
+    }
+  })
+  .controller("EquipoController", function ($scope, $http, peticion) {
+    //console.log(peticion.get());
+     peticion.get();
+  });
