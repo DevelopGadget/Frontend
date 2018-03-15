5ecb2e6f26aed9c941ddff732b2750ae6f88c69e
FernanPro18 <fernandoaraujo081@gmail.com>
2018-03-14 19:37:05 -0500

    pruebas

--- Js/ControllerJs/ControllerIndex.js
+++ Js/ControllerJs/ControllerIndex.js
@@ -1,29 +1,28 @@
 "use strict"
 angular.module("App", ["ngRoute"])
     .config(function ($routeProvider, $locationProvider) {
-        console.log('routeando');
         $routeProvider.when('/', {
                 templateUrl: 'index.html',
                 controller: 'IndexController'
             })
 
             .when('/Equipos', {
-                templateUrl: '/pages/Equipos.html',
+                templateUrl: '../pages/Equipos.html',
                 controller: 'ControllerEquipo'
             })
 
             .when('/EquiposReg', {
-                templateUrl: '/pages/EquiposReg.html',
+                templateUrl: '../pages/EquiposReg.html',
                 controller: 'EquipoController'
             })
 
             .when('/Jugadores', {
-                templateUrl: '/pages/Jugadores.html',
+                templateUrl: '../pages/Jugadores.html',
                 controller: 'ControllerJugadores'
             })
 
             .when('/JugadoresReg', {
-                templateUrl: '/pages/JugadoresReg.html',
+                templateUrl: '../pages/JugadoresReg.html',
                 controller: 'ControllerJugadores'
             })
 
