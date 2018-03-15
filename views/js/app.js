17aa471e279703af8de6727a897c6211671b17cb
Rober19 <texas_telecaster@hotmail.com>
2018-03-14 22:47:20 -0500

    v2

--- views/js/app.js
+++ views/js/app.js
@@ -2,8 +2,9 @@
 
 angular.module('myApp', ['appRoutes', 'ControllerEquipo', 'ControllerJugadores'])
   .controller('myController', function ($scope, $location) {
-    $scope.title = 'HAAA'
-    console.log('serví')
+    
+    $scope.title = 'HAAA';
+   
 
   });
 
