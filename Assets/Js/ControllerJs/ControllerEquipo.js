d5a69074e303d5094e7948d9e2faf6901f89ffd8
FernanPro18 <fernandoaraujo081@gmail.com>
2018-03-16 11:34:38 -0500

    modal equipos

--- Assets/Js/ControllerJs/ControllerEquipo.js
+++ Assets/Js/ControllerJs/ControllerEquipo.js
@@ -4,27 +4,66 @@ angular.module('EquipoController', ['ngResource'])
     var Equipos = {};
 
     Equipos.Res = $resource('https://api-net.herokuapp.com/api/Equipos/:id', {
-      id: "@id"
-    },
+        id: "@id"
+      },
 
-    { 'get':    {method:'GET'},
-      'save':   {method:'POST'},
-      'query':  {method:'GET', isArray:true},
-      'remove': {method:'DELETE'},
-      'put': {method:'PUT'}});
+      {
+        'get': {
+          method: 'GET'
+        },
+        'save': {
+          method: 'POST'
+        },
+        'query': {
+          method: 'GET',
+          isArray: true
+        },
+        'remove': {
+          method: 'DELETE'
+        },
+        'put': {
+          method: 'PUT'
+        }
+      });
 
     Equipos.getAll = function () {
       return Equipos.Res.query(function (data) {
         console.log(data);
+      }, function (error) {
+
       });
     };
 
-    Equipos.Post = function (data) {
-      Equipos.Res.save({notify:true}, data, function (resp) {
+    Equipos.Post = function (data, scope) {
+      Equipos.Res.save({
+        notify: true
+      }, data, function (resp) {
         console.log(resp);
-      }, function(error){
+        if (resp.statusCode == 200) {
+          scope.Modal = {
+            classBtn: 'btn btn-success',
+            classDiv: 'modal-dialog modal-notify modal-success',
+            Texto: 'Se Ha Registrado Satisfactoriamente',
+            Link : '#!/Equipos'
+          };
+        } else {
+          scope.Modal = {
+            classBtn: 'btn btn-danger',
+            classDiv: 'modal-dialog modal-notify modal-danger',
+            Texto: 'Ha Ocurrido Un Error Verifique Los Datos Ingresados',
+            Link : '#!/EquiposReg'
+          };
+        }
+      }, function (error) {
         console.log(error);
+        scope.Modal = {
+          classBtn: 'btn btn-danger',
+          classDiv: 'modal-dialog modal-notify modal-danger',
+          Texto: 'Ha Ocurrido Un Error Vuelva A Intentar',
+          Link : '#!/EquiposReg'
+        };
       });
+      $('#Modal').modal('show')
     }
 
     return Equipos;
@@ -34,10 +73,9 @@ angular.module('EquipoController', ['ngResource'])
     $scope.Equipos = Equipos.getAll();
     $scope.Array = {};
     $scope.Post = function () {
-      if($scope.form.$valid){
+      if ($scope.form.$valid) {
         console.log($scope.Array);
-        Equipos.Post($scope.Array);
+        Equipos.Post($scope.Array, $scope);
       }
     }
   });
-  
