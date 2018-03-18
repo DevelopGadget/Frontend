64029730c88ca6b5b0aa8f9eca413103ecaf76ee
FernanPro18 <fernandoaraujo081@gmail.com>
2018-03-17 19:51:08 -0500

    form jugador

--- Assets/Js/ControllerJs/ControllerJugadores.js
+++ Assets/Js/ControllerJs/ControllerJugadores.js
@@ -4,15 +4,43 @@ angular.module('JugadoresController', [])
     var Jugadores = {};
     Jugadores.Res = $resource('https://api-net.herokuapp.com/api/Jugadores/:id', {
       id: "@id"
+    },
+    {
+      'get': {
+        method: 'GET'
+      },
+      'save': {
+        method: 'POST'
+      },
+      'query': {
+        method: 'GET',
+        isArray: true
+      },
+      'delete': {
+        method: 'DELETE'
+      },
+      'put': {
+        method: 'PUT'
+      }
     });
 
-    Jugadores.getAll = function () {
+    Jugadores.ReadAll = function (scope) {
       return Jugadores.Res.query(function (data) {
+        scope.mostrar = true;
         console.log(data);
       });
     };
 
-    Jugadores.Post = function (data) {
+    Jugadores.Read = function (id) {
+      return Jugadores.Res.get({
+        id: id
+      }, function (data) {
+      }, function (error) {
+
+      })
+    };
+
+    Jugadores.Create = function (data) {
       return Jugadores.Res.save({
         data: data
       }, function (resp) {
@@ -22,12 +50,22 @@ angular.module('JugadoresController', [])
     return Jugadores;
   })
 
-  .controller('JugadoresController', function ($scope, Jugadores) {
-    $scope.Jugadores = Jugadores.getAll();
-    $scope.Post = function () {
-      $scope.Post = {};
+  .controller('JugadoresController', function ($scope, Jugadores, $routeParams, $window) {
+    $scope.mostrar = false;
+    $scope.Jugadores = Jugadores.ReadAll($scope);
+    $scope.Post = {};
+    if (String($routeParams.id) !== '' && String($routeParams.id) !== null && $routeParams.id !== undefined) {
+      $scope.copia = Jugadores.Read(String($routeParams.id));
+      $scope.Jugador = Jugadores.Read(String($routeParams.id));
+      $('#select').modal('show')
+    }
+    $('#select').on('hidden.bs.modal', function () {
+      $window.location.href = "#!/Jugadores";
+    });
+
+    $scope.Create = function () {
       if($scope.form.$valid){
-        Jugadores.Post(Post);
+        Jugadores.Create(Post);
       }
     }
   });
