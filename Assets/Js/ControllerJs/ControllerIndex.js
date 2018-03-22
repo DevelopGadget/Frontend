9b531b34ef473d8e2607b4e3a8f127aa9ef29ebe
FernanPro18 <fernandoaraujo081@gmail.com>
2018-03-22 12:35:05 -0500

    load index

--- Assets/Js/ControllerJs/ControllerIndex.js
+++ Assets/Js/ControllerJs/ControllerIndex.js
@@ -1,4 +1,23 @@
 angular.module('App', ['appRoutes', 'EquipoController', 'JugadoresController'])
-.controller('IndexController', function ($scope) {
+  .factory("HTTP", function ($http) {
+    var HTTP = {};
+    HTTP.Equipos = function(scope){
+      $http.get('https://api-net.herokuapp.com/api/Equipos').then(function (resp) {
+        scope.Equipos = resp.data;
+        scope.mostrar = true;
+      }, function (error) {
+        scope.mostrar = false;
+      });
+    }
+    return HTTP;
+  })
+  .controller('IndexController', function ($scope, HTTP) {
+    $scope.mostrar = false;
+    $scope.jugadores = false; 
+    $scope.mostrarLoad  = false; 
+    HTTP.Equipos($scope);
+    $scope.Seleccion = function (id) {
+      $scope.mostrarLoad  = true;
+    }
 
-});
+  });
