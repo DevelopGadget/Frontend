57a081186ce34ce5d391c23c3cdc17693882a279
FernanPro18 <fernandoaraujo081@gmail.com>
2018-03-24 17:27:39 -0500

    server

--- Server.js
+++ Server.js
@@ -1,15 +1,14 @@
 'use strict'
 
 const express = require('express');
+const path = require('path');
 const app = express();
 //
-app.use(express.static('Assets'));
+app.use(express.static(path.join(__dirname, 'Assets')));
 //
-
 app.get('/', (req, res) =>{
   res.sendfile(__dirname+'/Assets/pages/index.html');
 });
-
 app.listen(process.env.PORT || 2500, () => {
   console.log('Server Frontend Runing');
 });
