a7417c202f99b08d765402126425d0f6ee386c22
FernanPro18 <fernandoaraujo081@gmail.com>
2018-03-26 11:09:43 -0500

    heroku

--- Server.js
+++ Server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+//
+app.use(express.static('assets'));
+//
+app.get('/', (req, res) =>{
+  res.sendfile(__dirname+'/assets/pages/index.html');
+});
+app.listen(process.env.PORT || 2500, () => {
 
-express()
-  .use(express.static(path.join(__dirname, 'Assets')))
-  .set('Pages', path.join(__dirname, '/Assets/Pages'))
-  .set('js', path.join(__dirname, '/Assets/js'))
-  .set('Css', path.join(__dirname, '/Assets/Css'))
-  .set('font', path.join(__dirname, '/Assets/font'))
-  .set('scss', path.join(__dirname, '/Assets/scss'))
-  .get('/', (req, res) => res.sendfile(__dirname+'/Assets/Pages/index.html'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  console.log('Server Frontend Runing');
+
+});
