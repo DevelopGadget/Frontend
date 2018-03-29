f0129eb58d1057a6851b7802336d61ed28ec6438
FernanPro18 <fernandoaraujo081@gmail.com>
2018-03-28 21:53:38 -0500

    Final

--- Server.js
+++ Server.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'assets')))
-  .get('/', (req, res) => res.sendfile(__dirname+'/Assets/Pages/index.html'))
+  .get('/', (req, res) => res.sendfile(__dirname+'/assets/pages/index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
